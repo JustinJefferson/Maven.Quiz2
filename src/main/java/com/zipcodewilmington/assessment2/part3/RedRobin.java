414995ffc2669767f718b2d03f82b16641232fa5
Justin <justleland@gmail.com>
2019-06-10 11:27:59 -0400

    Finished Part 3. Going back to complete Part 2

--- src/main/java/com/zipcodewilmington/assessment2/part3/RedRobin.java
+++ src/main/java/com/zipcodewilmington/assessment2/part3/RedRobin.java
@@ -1,4 +1,22 @@
 package com.zipcodewilmington.assessment2.part3;
 
-public class RedRobin {
+public class RedRobin extends Bird{
+    private String migrationMonth;
+
+    public RedRobin() {
+        migrationMonth = "";
+    }
+
+    public int getSpeed() { return 10; }
+    public String color() {return "red";}
+
+    @Override
+    public String getMigrationMonth() {
+        return migrationMonth;
+    }
+
+    @Override
+    public void setMigrationMonth(String migrationMonth) {
+        this.migrationMonth = migrationMonth;
+    }
 }
